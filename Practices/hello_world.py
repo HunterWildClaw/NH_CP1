0bf2c6b927e53f62e53db55676b9d4cc3e69bf38
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-08-20 10:43:36 -0600

    I finished Hello World

--- Practices/hello_world.py
+++ Practices/hello_world.py
@@ -1,1 +1,9 @@
 # NH 2nd Hello World
+
+name = input("Hello there! What's your name?: ")
+if name == "Nathan":
+    print("Hello Admin,", name)
+elif name == "LaRose":
+    print("Hello Teach!")
+else:
+    print("Welcome", name)
