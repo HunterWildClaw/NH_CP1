4dcfc59a759f9bba65e1dd7ca44905a701a88698
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-08-22 10:51:22 -0600

    I did more stuff in variables notes and did Who are you.

---
+++ Practices/Who_are_you.py
@@ -0,0 +1,6 @@
+# NH 2nd Who are you?
+
+name = input("Hello there! Who are you?: ")
+age = input("Oh and--- not to creepy or anything ---but how old are you?: ")
+color = input("And just a simple question to get to know you, what's your favorite color?: ")
+print("Hello,", name, "you're", age, "years old, and your favorite color is", color + ".")
