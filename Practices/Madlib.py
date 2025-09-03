d0898e813469ec5c9cdbfdfb7c785add47ab1903
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-03 10:50:59 -0600

    I did madlib

---
+++ Practices/Madlib.py
@@ -0,0 +1,9 @@
+# NH 2nd Madlib
+
+adjective = input("Name a random advective: ")
+noun = input("Oh, and a random noun. Just for funsies: ")
+name_of_brother = input("And what's a masculine name you can think of?: ")
+verb = input("And what's some past tensed verb you can think of off the top of your head?: ")
+adjective_1 = input("And last but not least, what's another advective?: ")
+
+print("Ok here is a mathlib based off of what you have given me so far.\n It was a nice, " + adjective + " day. As you got out of bed, you tripped on a " + noun + " and " + verb + " to the floor. Your brother, " + name_of_brother + " came in and asked if you were ok. You said you were fine and " + verb + " past him and down the stairs and sat down for breakfast. Mom " + verb + " past you and dropped a plate full of " + adjective_1 + " cereal. You took a bite and passed out. The end.")
