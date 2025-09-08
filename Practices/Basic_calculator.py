963fd93f2f8664d3c733addce532bd386b7a9ac5
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-08 10:51:36 -0600

    I took notes for outputs and did basic calcualtr and tried to finish idiot proof but failed again.,

---
+++ Practices/Basic_calculator.py
@@ -0,0 +1,11 @@
+# NH 2nd Basic Calculator
+
+num = float(input("Yo what's up my homie! Give me some random number off the top of your head. "))
+num_1 = float(input("And I need another one to make this trick work. "))
+
+print(num + num_1)
+print(num - num_1)
+print(num * num_1)
+print(num / num_1)
+print(num ** num_1)
+print(num % num_1)
--- Practices/Idiot_Proof.py
+++ Practices/Idiot_Proof.py
@@ -3,3 +3,8 @@
 name = input("What's your name? I promise I'm not a stalker. ")
 phone_number = input("I swear I'm not stalking you... But what's your phone number? ")
 gpa = input("Great! Now give me your gpa. ")
+
+name = name.strip ("  ")
+name = name.title
+print(name)
+phone_number = phone_number.replace (" ")
