577b118eeb7bfd18240605cb2d4dedd84ec7f097
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-10 10:51:30 -0600

    we did notes on random nums and I started crew shares

---
+++ Practices/Crew_Shares.py
@@ -0,0 +1,7 @@
+# 2nd NH Crew Share
+
+total_money = input("Hey so name a random number. (And this works better if it's a big number.) Enter number: ")
+
+print("So here's the story.\nThe crew earned a whole bunch of money on the last outing, but the captain didn't have time to divvy it all up before release everyone to port. He gave each member of the crew 500 dollars for the evening and then sat down with his first mate to properly divide the shares. ")
+print("You're number you gave is the total ammount of money. Now we split it up. *cracks knuckles and pops neck* Watch me cook")
+
