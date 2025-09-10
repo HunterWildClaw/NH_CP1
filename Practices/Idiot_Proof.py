eb6115a351ee5b849f1a8286e7f7a83f60f568ba
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-10 10:01:42 -0600

    I didn't do anything but it says I did. So I did something in Idiot Proof

--- Practices/Idiot_Proof.py
+++ Practices/Idiot_Proof.py
@@ -4,9 +4,9 @@ name = input("What's your name? I promise I'm not a stalker. ")
 phone_number = input("I swear I'm not stalking you... But what's your phone number? ")
 gpa = input("Great! Now give me your gpa. ")
 
-name = name.strip("  ").title
+name = name.strip().title()
 print(name)
 phone_number = phone_number.replace("-"," ")
 
 
-print(f"Hello {name}. I can now call you with the number {phone_number.replace("-"," ")}")
+print(f"Hello {name}. I can now call you with the number {phone_number.replace("-"," ")}. And you have a miserable {gpa} gpa. Oof.")
