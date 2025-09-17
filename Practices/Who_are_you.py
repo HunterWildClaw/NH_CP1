862abe31b20c063d6b9d2997c7e210ad3111442b
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-17 10:40:01 -0600

    So I logged in on different computer and I also deleted a file I didn't need.  This is also the first commit and push on this new computer.

--- Practices/Who_are_you.py
+++ Practices/Who_are_you.py
@@ -3,4 +3,4 @@
 name = input("Hello there! Who are you?: ")
 age = input("Oh and--- not to creepy or anything ---but how old are you?: ")
 color = input("And just a simple question to get to know you, what's your favorite color?: ")
-print("Hello,", name, "you're", age, "years old, and your favorite color is", color + ".")
+print("Hello,", name + ". You're", age, "years old, and your favorite color is", color + ".")
