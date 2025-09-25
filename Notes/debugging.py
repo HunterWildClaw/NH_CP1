a78e5be38e5efdcf41468fdfe829d2392a76cc83
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-25 10:51:04 -0600

    grrr my computer is still broken and we did debugging

---
+++ Notes/debugging.py
@@ -0,0 +1,6 @@
+# NH 2nd debugging notes. (I need it...)
+
+#Syntax, Run-Time, and Logic errors.
+#Sigghhh. Looks like this isn't what I need.
+#Fix by reading where problem is and fixing it. For Syntax
+#Fix 
