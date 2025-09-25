9ca96f0a0232ca5d753d55f472aeed7dbec1bc30
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-25 10:12:51 -0600

    Testing

--- Practices/Idiot_Proof.py
+++ Practices/Idiot_Proof.py
@@ -9,3 +9,4 @@ print(name)
 phone_number = phone_number.replace("-"," ")
 
 print(f"Hello {name}. I can now call you with the number {phone_number}. And you have a miserable {gpa:.2f} gpa. Oof.")
+print("Something")
