b9c86f2424bddf9db97bf0003ad21cf2b2e33676
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-09-26 10:51:08 -0600

    I tried to finish fix the game but failed. We did debugging notes.

--- Notes/debugging.py
+++ Notes/debugging.py
@@ -1,6 +1,14 @@
 # NH 2nd debugging notes. (I need it...)
 
-#Syntax, Run-Time, and Logic errors.
+#Syntax, Logic, and Run-Time errors.
 #Sigghhh. Looks like this isn't what I need.
 #Fix by reading where problem is and fixing it. For Syntax
-#Fix 
+#Fix by working through you code step by step. Rubber-duck debugging. For Logic
+#Fix by using the debugger.
+
+import random
+
+while True:
+    denominator = random.randint(0,5)
+
+    print(10/denominator)
