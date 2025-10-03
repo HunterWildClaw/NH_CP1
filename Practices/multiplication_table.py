8e4b9b67cc5de2900157a7f6e27f7155142d4bb0
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-03 10:48:29 -0600

    I did while loops notes and editied multi table some more and strarted rock paper scissors

--- Practices/multiplication_table.py
+++ Practices/multiplication_table.py
@@ -1,4 +1,15 @@
 # NH 2nd Mulitplication Table
-
+import time
+print("Welcome User! Here at Multi Table, we do all your multiplication for you. And here's a free demo table!")
+time.sleep(3)
 
 for x in range(1,13,1): print(f"{1*x}\t{2*x}\t{3*x}\t{4*x}\t{5*x}\t{6*x}\t{7*x}\t{8*x}\t{9*x}\t{10*x}\t{11*x}\t{12*x}\n")
+time.sleep(5)
+    
+num_1 = input("Now, what multiplication problem troubles you? I can help.\nInsert the first number of your problem: ")
+num_2 = input("Hmm ok. Now insert second number: ")
+time.sleep(1)
+print("Ok I think I got it. Gimme a sec.")
+time.sleep(1)
+answer = num_1*num_2
+print(f"Ah ha! The answer to your question is {answer}!")
---
+++ Practices/rock_paper_scissors.py
@@ -0,0 +1,8 @@
+# NH 2nd Rock Paper Scissors
+import random
+import time
+
+while True:
+    choice = input("Hello! Would you like to play Rock, Paper, Scissors? Or would you like to quit. Push 'p' to play and 'q' to quit").lower().strip()
+    if choice == p:
+        print("Alright. You have chosen death!")
