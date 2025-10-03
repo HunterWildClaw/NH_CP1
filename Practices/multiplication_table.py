c72706e0bfb6aa028a6f1716295f70f2774fdcf4
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-03 09:59:29 -0600

    I edited multi table

--- Practices/multiplication_table.py
+++ Practices/multiplication_table.py
@@ -1,4 +1,4 @@
 # NH 2nd Mulitplication Table
 
 
-for x in range(1,13,1): print(f"{1*x}\t{2*x}\t{3*x}\t{4*x}\t{5*x}\t{6*x}\t{7*x}\t{8*x}\t{9*x}\t{10*x}\t{11*x}\t{12*x}\t\n")
+for x in range(1,13,1): print(f"{1*x}\t{2*x}\t{3*x}\t{4*x}\t{5*x}\t{6*x}\t{7*x}\t{8*x}\t{9*x}\t{10*x}\t{11*x}\t{12*x}\n")
