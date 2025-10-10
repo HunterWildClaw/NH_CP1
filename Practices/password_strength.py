c66b9dd2bef5bcc9158d94a24063cb835835d11b
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-10 10:51:09 -0600

    I did tic tac toe and am now working on password checker.

---
+++ Practices/password_strength.py
@@ -0,0 +1,24 @@
+#NH 2nd Password Strength Checker
+
+# Get password from user by asking them for it and make it a variable called PASSWORD.
+password = input("What's your password? ")
+
+# Check Password to make sure it at least had 8 chars in it. Then show how many characters it has in it. Do this by creating a line of code that has a new variable called PASSWORD LENGTH and define the variable as the length of the password. len(Password) will work. Then show how many characters it has in it so that you know.
+password_length = len(password)
+print("You password has "+password_length+" characters in it.")
+
+# Check PASSWORD to make sure it has at least one uppercase letter in it. Then show how many uppercase letters it has.
+
+# Check PASSWORD to make sure it had at least one lowercase letter in it. Then show how many lowercase letters it has.
+
+# Check PASSWORD to make sure it has at least one number in it. Then show
+
+# Check PASSWORD to make sure it has at least one special character in it.
+
+# If PASSWORD has at least 8 characters, one uppercase letter, one lower case letter, one number, and one special character in it, then show that it is a very strong password.
+
+# If one of those attributes are false, then say that it is a strong password.
+
+# If two of those attributes are false, then show that it's a moderate password. 
+
+# If three or four of those attributes are false, then show that it's a weak password.
