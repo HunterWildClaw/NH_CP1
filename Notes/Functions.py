ce03117df462c24ec120658372ff4f2c5bae1a1f
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-20 10:51:31 -0600

    we did fiuncitons notes

---
+++ Notes/Functions.py
@@ -0,0 +1,13 @@
+#NH 2nd Functions notes
+num = 0
+def add(x, y):
+    return x+y
+
+while num < add(5,5):
+    print("Duck")
+    num += 1
+print("Goose")
+print(f"Results is : {add(-6519146, 36516)}")
+total = add(689461, 654894)
+print(add(add(3.14, .85)))
+add(42, 7)
