02fecbac7fcd6f4e30b07d7c190cdc63445013d7
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-25 02:16:32 +0000

    updaTED combAt

--- Practices/combat.py
+++ Practices/combat.py
@@ -5,7 +5,7 @@ player_def = 0
 player_atk = 0
 player_hp = 0
 player_name = input("Welcome to the Training Dojo! Let's get to know you. \nWhat's your name? ")
-player_class = input(f"Hello, {player_name}! It's good to meet you. What class are you?\nPress '1' to be a Warrior! \nPress '2' to be a Mage! \nPress '3' to be a Rogue! \nPress '4' to be a Druid! \nOr press '5' to be a Hunter!")
+player_class = input(f"Hello, {player_name}! It's good to meet you. What class are you?\nPress '1' to be a Warrior! \nPress '2' to be a Mage! \nPress '3' to be a Rogue! \nOr press '4' to be a Hunter!")
 if player_class == '1':
     print("Alright! You are a Warrior! Generating your stats now...")
     sleep.sleep(1)
@@ -37,12 +37,12 @@ elif player_class == '3':
     rogue_def=player_def
     print(f"Your stats are {rogue_hp} hp, {rogue_atk} attack, and {rogue_def} defense.")
 elif player_class == '4':
-    print("Alright! You are a Druid! Generating your stats now...")
+    print("Alright! You are a Hunter! Generating your stats now...")
     sleep.sleep(1)
-    player_hp += 40
-    rogue_hp = player_hp
+    player_hp += 35
+    hunter_hp = player_hp
     player_atk += 40
-    rogue_atk=player_atk
+    hunter_atk=player_atk
     player_def += 30
-    rogue_def=player_def
-    print(f"Your stats are {rogue_hp} hp, {rogue_atk} attack, and {rogue_def} defense.")
+    hunter_def=player_def
+    print(f"Your stats are {hunter_hp} hp, {hunter_atk} attack, and {hunter_def} defense.")
