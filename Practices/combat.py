a3047ee9bec0dbb0f8af2a63bf4548a881562c74
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-27 10:51:04 -0600

    I tried on combat again.

--- Practices/combat.py
+++ Practices/combat.py
@@ -2,22 +2,6 @@
 import time as sleep
 import random
 
-def player_action(health, defense, damage):
-    player_move = input("Ok, so. You were wandering in the dark woods when a fierce spiderling leaps at you from above. You dodge and roll to the side. Your health potions clink against your defense potions. You also remember your rage potions to increase the power of your next hit. Would you like to drink one of the potions? Or just fight? Press '1' to drink and '2' to fight: ")
-    if player_move == '1':
-        player_potion_choice = input("Would you like to drink a potion to increase your health, defense, or attack? press '1' for health, '2' for defense, or '3' for attack: ")
-        if player_potion_choice == '1':
-            player_hp += health_potion
-            print(f"You now have {player_hp} health!")
-        elif player_potion_choice == '2':
-            player_def += defense_potion
-            print(f"You now have {player_def} defense points!")
-        elif player_potion_choice == '3':
-            player_atk += rage_potion
-            print(f"You now have {player_atk} attack power!")
-        else:
-            print("Please do '1', '2', or '3'. Do not troll.")
-
 monster_hp = 50
 monster_atk = 10
 monster_def = 20
@@ -66,23 +50,64 @@ while player_class:
         print("Please press 1, 2, 3, or 4. Do not troll.")
         break
 
+
 sleep.sleep(1)
-monster=''
-player=''
+monster='Monster'
+player='Player'
 turn_determination = random.randint(1,2)
 print(turn_determination)
 if turn_determination == 1:
     turn_determination=monster
 elif turn_determination==2:
     turn_determination==player
+else:
+    print("Error!!!")
 
-
-if turn_determination == monster:
+if turn_determination == monster or 1:
     if monster_atk > player_def:
         player_hp -= monster_atk
         print(f"Ok, so. You were wandering in the dark woods when a fierce spiderling leaps at you from above and scratches your head. It deals {monster_atk} damage! You now have {player_hp} health!")
     elif monster_atk < player_def:
-        player_action(player_hp, player_def, player_atk)
-elif turn_determination == player:
-    player_turn = player_action(player_hp, player_def, player_atk)
-    print(player_turn)
+        player_move = input("Ok, so. You were wandering in the dark woods when a fierce spiderling leaps at you from above. You dodge and roll to the side. Your health potions clink against your defense potions. You also remember your rage potions to increase the power of your next hit. Would you like to drink one of the potions? Or just fight? Press '1' to drink and '2' to fight: ")
+        if player_move == '1':
+            player_potion_choice = input("Would you like to drink a potion to increase your health, defense, or attack? press '1' for health, '2' for defense, or '3' for attack: ")
+            if player_potion_choice == '1':
+                player_hp += health_potion
+                print(f"You now have {player_hp} health!")
+            elif player_potion_choice == '2':
+                player_def += defense_potion
+                print(f"You now have {player_def} defense points!")
+            elif player_potion_choice == '3':
+                player_atk += rage_potion
+                print(f"You now have {player_atk} attack power!")
+            else:
+                print("Please do '1', '2', or '3'. Do not troll.")
+        elif player_move == '2':
+            if player_atk > monster_def:
+                monster_hp -= player_atk
+                print(f"You hit! The monster now has {monster_hp} health!")
+            elif player_atk < monster_def:
+                print("You missed! L bozo")
+elif turn_determination == player or 2:
+    player_move = input("Ok, so. You were wandering in the dark woods when a fierce spiderling leaps at you from above. You dodge and roll to the side. Your health potions clink against your defense potions. You also remember your rage potions to increase the power of your next hit. Would you like to drink one of the potions? Or just fight? Press '1' to drink and '2' to fight: ")
+    if player_move == '1':
+        player_potion_choice = input("Would you like to drink a potion to increase your health, defense, or attack? press '1' for health, '2' for defense, or '3' for attack: ")
+        if player_potion_choice == '1':
+            player_hp += health_potion
+            print(f"You now have {player_hp} health!")
+        elif player_potion_choice == '2':
+            player_def += defense_potion
+            print(f"You now have {player_def} defense points!")
+        elif player_potion_choice == '3':
+            player_atk += rage_potion
+            print(f"You now have {player_atk} attack power!")
+        else:
+            print("Please do '1', '2', or '3'. Do not troll.")
+    elif player_move == '2':
+        if player_atk > monster_def:
+            monster_hp -= player_atk
+            print(f"You hit! The monster now has {monster_hp} health!")
+        elif player_atk < monster_def:
+            print("You missed! L bozo")
+else:
+    print('Error! Something went wrong!')
