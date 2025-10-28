94c85f36cab9c459733191b02f68171430f369e6
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-28 10:51:08 -0600

    I did race turtles and we did turtles notes on test.py.

---
+++ Practices/race_turtles.py
@@ -0,0 +1,19 @@
+# NH 2nd Turtle Racing
+#Import all the libraries
+import turtle
+import random
+
+# Set speed of all turtles
+turt_1_speed = random.randint
+turt_2_speed = random.randint
+turt_3_speed = random.randint
+turt_4_speed = random.randint
+turt_5_speed = random.randint
+
+# Make the turtles
+Michelangelo = turtle.Turtle()
+Raphael = 
+
+
+turtle.penup()
+turtle.goto(-100, 50)
