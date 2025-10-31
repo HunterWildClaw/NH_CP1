e92524500e4895a38b88e23f04a497bb3b7b7bf8
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-10-31 10:51:00 -0600

    I dont like this game anymore!!!!

---
+++ Practices/maze.generator.py
@@ -0,0 +1,40 @@
+# NH 2nd Maze Generator
+# Import random as dum
+import random as dum
+import turtle as bob
+
+#Make the guy that makes the maze
+turtle = bob.Turtle()
+
+#Make maze generator as a function
+def maze_generation():
+    #Make the rows
+    rows = [
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          []
+    ]   
+    #Make the collumns
+    cols = [
+           [],
+           [],
+           [],
+           [],
+           [],
+           []
+    ]
+def randomnation(rows):
+    for row in rows:
+        walls = ""
+
+
+
+
+
+
+
+bob.done()
