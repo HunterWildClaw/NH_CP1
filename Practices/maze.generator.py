bb8de236679f256fbd0f648a7aa2b0a58335ba99
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-11-04 10:51:09 -0700

    I HATE CODING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Practices/maze.generator.py
+++ Practices/maze.generator.py
@@ -1,19 +1,8 @@
 # NH 2nd Maze Generator
 # Import random as dum
 import random as dum
+# Import turtle as bob
 import turtle as bob
-
-#Make the guy that makes the maze
-maze_maker = bob.Turtle()
-
-#Make the screen
-screen = bob.Screen()
-screen.setup(600, 600)
-#Make maze generator as a function
-def maze_generation():
-    #Make the rows
-    make_map = [[1,0,1,1,1,1],[],[],[],[],[],[],[1,0,1,1,1,1]]
-
 
 # Make a function to test solveabilty of maze
 def is_solveable(row_grid, col_grid):
@@ -44,6 +33,50 @@ def is_solveable(row_grid, col_grid):
         if y > 0 and row_grid[y][x] == 0:
             stack.append((x, y-1))
 
-    return False        
+    return False
+
+
+#Make maze generator as a function
+def maze_generation():
+    #Make the rows
+    grid_rows = [[1,1,0,1,1,1],
+                 [1,1,1,1,0,1],
+                 [1,0,0,1,1,1],
+                 [1,1,0,1,0,1],
+                 [1,1,0,0,0,1],
+                 [1,1,1,0,1,1]]
+    for wall in grid_rows:
+        if wall == 1:
+            bob.pendown()
+            bob.forward(100)
+
+        elif wall == 0:
+            bob.penup()
+            bob.forward(100)
+
+    #Make the collumns
+    bob.right(90)
+    grid_cols = [[1,1,1,1,1,1],
+                 [1,0,0,1,1,1],
+                 [1,0,0,1,0,1],
+                 [1,0,1,0,1,1],
+                 [1,1,0,1,1,1],
+                 [1,0,1,1,0,1]]
+    for wal in grid_cols:
+        if wal == 1:
+            bob.pendown()
+            bob.forward(100)
+
+        elif wal == 0:
+            bob.penup()
+            bob.forward(100)
+
+#Make the screen
+screen = bob.Screen()
+screen.setup(600, 600)
+
+#Make the maze
+bob.speed(0)
+maze_generation()
 
 bob.done()
