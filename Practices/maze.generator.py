eb22f29d9659c89caf92bba51a114e569adedbc7
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-11-07 10:52:43 -0700

    I FINSIHED!

--- Practices/maze.generator.py
+++ Practices/maze.generator.py
@@ -26,7 +26,7 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
     
-    row_1=dum.randint(0,1)
+    row_1=0
     if row_1==0:
         bob.penup()
         bob.forward(100)
@@ -50,7 +50,7 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
-    row_4=dum.randint(0,1)
+    row_4=0
     if row_4==0:
         bob.penup()
         bob.forward(100)
@@ -83,14 +83,14 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
-    row_8=dum.randint(0,1)
+    row_8=0
     if row_8==0:
         bob.penup()
         bob.forward(100)
     elif row_8==1:
         bob.pendown()
         bob.forward(100)
-
+    
     row_9=dum.randint(0,1)
     if row_9==0:
         bob.penup()
@@ -116,7 +116,7 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
-    row_12=dum.randint(0,1)
+    row_12=0
     if row_12==0:
         bob.penup()
         bob.forward(100)
@@ -148,7 +148,7 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
-    row_16=dum.randint(0,1)
+    row_16=0
     bob.teleport(0,-400)
     if row_16==0:
         bob.penup()
@@ -181,7 +181,7 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
-    row_20=dum.randint(0,1)
+    row_20=0
     if row_20==0:
         bob.penup()
         bob.forward(100)
@@ -198,7 +198,7 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
-    row_22=dum.randint(0,1)
+    row_22=1
     if row_22==0:
         bob.penup()
         bob.forward(100)
@@ -214,7 +214,7 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
-    row_24=dum.randint(0,1)
+    row_24=1
     if row_24==0:
         bob.penup()
         bob.forward(100)
@@ -235,13 +235,14 @@ def maze_generation():
     bob.teleport(100, 0)
     #Make it face down
     bob.setheading(270)
-    col_1=dum.randint(0,1)
+    col_1=0
     if col_1==0:
         bob.penup()
         bob.forward(100)
     elif col_1==1:
         bob.pendown()
         bob.forward(100)
+
     col_2=dum.randint(0,1)
     if col_2==0:
         bob.penup()
@@ -267,7 +268,7 @@ def maze_generation():
         bob.forward(100)
 
 
-    col_5=dum.randint(0,1)
+    col_5=0
     if col_5==0:
         bob.penup()
         bob.forward(100)
@@ -275,55 +276,181 @@ def maze_generation():
         bob.pendown()
         bob.forward(100)
 
+    col_6=0
+    bob.teleport(200,0)
+    if col_6==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_6==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_7=dum.randint(0,1)
+    if col_7==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_7==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_8=dum.randint(0,1)
+    if col_8==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_8==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_9=dum.randint(0,1)
+    if col_9==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_9==1:
+        bob.pendown()
+        bob.forward(100)
 
 
+    col_10=dum.randint(0,1)
+    if col_10==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_10==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_11=0
+    bob.teleport(300,0)
+    if col_11==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_11==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_12=dum.randint(0,1)
+    if col_12==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_12==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_13=dum.randint(0,1)
+    if col_13==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_13==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_14=dum.randint(0,1)
+    if col_14==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_14==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_15=0
+    if col_15==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_15==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_16=dum.randint(0,1)
+    bob.teleport(400,0)
+    if col_16==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_16==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_17=dum.randint(0,1)
+    if col_17==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_17==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_18=dum.randint(0,1)
+    if col_18==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_18==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_19=0
+    if col_19==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_19==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_20=dum.randint(0,1)
+    if col_20==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_20==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_21=dum.randint(0,1)
+    bob.teleport(500,0)
+    if col_21==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_21==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_22=dum.randint(0,1)
+    if col_22==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_22==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_23=dum.randint(0,1)
+    if col_23==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_23==1:
+        bob.pendown()
+        bob.forward(100)
+
+    col_24=0
+    if col_24==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_24==1:
+        bob.pendown()
+        bob.forward(100)
 
 
+    col_25=dum.randint(0,1)
+    if col_25==0:
+        bob.penup()
+        bob.forward(100)
+    elif col_25==1:
+        bob.pendown()
+        bob.forward(100)
+    
+    walls = row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8, row_9, row_10, row_11, row_12, row_13, row_14, row_15, row_16, row_17, row_18, row_19, row_20, row_21, row_22, row_23, row_24, row_25, col_1, col_2, col_3, col_4, col_5, col_6, col_7, col_8, col_9, col_10, col_11, col_12, col_13, col_14, col_15, col_16, col_17, col_18, col_19, col_20, col_21, col_22, col_23, col_24, col_25
+    return walls
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def solveability_check():
+    solver=bob.Turtle()
+    solver.color("#FF8800")
+    solver.penup()
+    solver.goto(50,0)
+    
 
 def border_generation():
     bob.speed(0)
@@ -343,11 +470,11 @@ def border_generation():
     bob.right(90)
     bob.forward(600)
 
-
+solveability_check()
 border_generation()
 maze_generation()
 
 
 
-
+bob.hideturtle()
 bob.done()
