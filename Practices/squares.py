b0dcb4e56c5b7a562a10f1e83df6d57b4aee9586
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-11-20 10:18:20 -0700

    I finished!!!

--- Practices/squares.py
+++ Practices/squares.py
@@ -1,8 +1,5 @@
-#NH 2nd Squared Numbers
-
-numbers=[3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
-
-squares=list(map(lambda num: num*num, numbers))
-
-for i, num in enumerate(numbers):
-    print(f"{num} squared is {squares[i]}")
+#NH 2nd Squared Numbers 
+#Make a list of nums
+numbers=[3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285] # Now make a thing to square all those numbers. Then use the lambda thing to perform the calculation
+squares=list(map(lambda num: num*num, numbers)) # Now print out all of the outcomes
+for i, num in enumerate(numbers): print(f"{num} squared is {squares[i]}")
