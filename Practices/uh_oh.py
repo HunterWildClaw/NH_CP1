f61bb36d449bbc5696799c5b4b638eb2e44ac5fb
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-11-24 10:52:26 -0700

    I tried again on uh oh

--- Practices/uh_oh.py
+++ Practices/uh_oh.py
@@ -1,24 +1,7 @@
 #NH 2nd factorial pseduocode
 
-#define function called factor
-def factor():
-    #while it so that it loops
-    while True:
-        #input asking for a number
-        number=int(input("Give a random number off the top of your head: "))
-        #check if the input is an integer
-        if number is int:
-            #once integer is inputed show the number
-            print(number)
-        #And use a for statement to get every number <0 before number and multiply them all.
-            for i in number:
-                i=number-1
-                if i==0:
-                    break
-                else:
-                    number*i   
-        #loops over if not
-        elif number is not int:
-            continue
-
-factor()
+#Make a funciton called factor
+def factor(num):
+    while True: #Make a while loop
+        num_1=num-1
+        num_2=num-2
