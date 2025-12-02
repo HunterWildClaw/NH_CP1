c6e0fd7fefeca17cb138f9f4eb75ea5d4ee68050
HunterWildClaw <nathan.hasson@ucas-edu.net>
2025-12-02 10:50:31 -0700

    idk *shrugs*

--- Practices/tic_tac_toe.py
+++ Practices/tic_tac_toe.py
@@ -7,25 +7,24 @@ player_options = ()
 
 if 1 and 2 and 3 in player_options:
     print("You win!")
-    break
+
 elif 4 and 5 and 6 in player_options:
     print("You win!")
-    break
+
 elif 7 and 8 and 9 in player_options:
     print("You win!")
-    break
+
 elif 1 and 5 and 9 in player_options:
     print("You win!")
-    break
+
 elif 7 and 5 and 3 in player_options:
     print("You win!")
-    break
+
 elif 1 and 4 and 7 in player_options:
     print("You win!")
-    break
+
 elif 2 and 5 and 8 in player_options:
     print("You win!")
-    break
+    
 elif 3 and 6 and 9 in player_options:
     print("You win!")
-    break
